66afd69534aa257baffd24cd2057c6ec6fb8de72
jeonhoeun <jeonhoeun@naver.com>
2021-12-13 17:50:35 +0900

    get concrete number usecase test red

---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,16 @@
+import 'package:clean_arch_tdd/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+class MockNumberTriviaRepository extends Mock implements NumberTriviaRepository{
+
+}
+
+void main(){
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+  setUp((){
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+}
